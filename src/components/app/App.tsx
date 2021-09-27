058b661360502cf4565397ad4a3b042e6939994d
Gerog-code <jornig@outlook.com>
2021-09-27 23:57:46 +0200

    still has weird bugs

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,15 +2,37 @@ import Button from "../button/button";
 import RoomDisplay from "../room-display/room-display";
 import "./app.scss";
 import HeroImg from "../../assets/lost.svg";
-//
-import dayInput from "../../assets/dates/1.json";
+import mon from "../../assets/dates/1.json";
+import tue from "../../assets/dates/2.json";
+import wed from "../../assets/dates/3.json";
+import thu from "../../assets/dates/4.json";
+import fri from "../../assets/dates/5.json";
+
 import { useState } from "react";
 
 function App() {
   const [rooms, setRooms] = useState(["Klick", "To", "Load"]);
 
+  function getData() {
+    let now = new Date();
+    if (now.getDay() === 1) {
+      return mon as Record<string, string[]>;
+    } else if (now.getDay() === 2) {
+      return tue as Record<string, string[]>;
+    } else if (now.getDay() === 3) {
+      return wed as Record<string, string[]>;
+    } else if (now.getDay() === 4) {
+      return thu as Record<string, string[]>;
+    } else if (now.getDay() === 5) {
+      return fri as Record<string, string[]>;
+    } else {
+      return {};
+    }
+  }
+  // rework that shit
+  // TODO #1 fix the 22:30 on 23:10 bug
   function time() {
-    const day = dayInput as Record<string, string[]>;
+    const day = getData();
 
     let now = new Date();
 
@@ -34,6 +56,7 @@ function App() {
       }
 
       if (timeInterval !== undefined && timeInterval >= 0) {
+        console.log(day[times[timeInterval]]);
         return day[times[timeInterval]];
       }
     }
@@ -50,20 +73,34 @@ function App() {
   return (
     <div className='app'>
       <div className='app__flex'>
-        <div className='app__flex-container-container'>
-          <div className='app__input-container'>
-            <h1>Find a Room</h1>
-            <div className='app__roomdisplay'>
-              {rooms.map((room) => (
-                <RoomDisplay name={room} />
-              ))}
-            </div>
-            <Button OnClick={lol} />
+        <div
+          className={
+            rooms.length > 5
+              ? "app__input-container app__wide"
+              : "app__input-container"
+          }
+        >
+          <h1>Find a Room</h1>
+          <div className='app__roomdisplay'>
+            {rooms.map((room) => (
+              <RoomDisplay name={room} />
+            ))}
           </div>
+          <Button OnClick={lol} />
         </div>
 
-        <div className='app__svg'>
-          <img src={HeroImg} alt='main_img' className='app__svg-img' />
+        <div
+          className='app__svg'
+          style={{ width: rooms.length > 5 ? "0px" : "" }}
+        >
+          <img
+            src={HeroImg}
+            alt='main_img'
+            className='app__svg-img'
+            style={{
+              display: rooms.length > 5 ? "none" : "",
+            }}
+          />
         </div>
       </div>
       <p className='app__footer'>

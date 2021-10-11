54b9d703f72447794c20e1435c07e79ec07b057c
Gerog-code <jornig@outlook.com>
2021-10-11 16:09:12 +0200

    rewrite step 1

---
+++ src/routes/home/home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.scss";
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='home__content'>
+        <p>Lolololo</p>
+      </div>
+    </div>
+  );
+}
+export default Home;

5342f3c9dd36a7b9ff25208b6e8abbe74def5802
Gerog-code <jornig@outlook.com>
2021-10-15 22:44:24 +0200

    dropdown

---
+++ src/shared/components/dropdown/dropdown.tsx
@@ -0,0 +1,13 @@
+import "./dropdown.scss";
+
+function Dropdown() {
+  return (
+    <div className='dropdown'>
+      <select>
+        <option>Zeit Wählen</option>
+      </select>
+    </div>
+  );
+}
+
+export default Dropdown;

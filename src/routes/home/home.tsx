d55dbf8f0ff368fff4559c2d5ea020f39fcf7e68
Gerog-code <jornig@outlook.com>
2021-10-15 22:26:08 +0200

    hero

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./home.scss";
 import Navbar from "../../shared/components/navbar/navbar";
+import TimeSelect from "../../shared/components/timeSelect/timeSelect";
 
 function Home() {
   return (
     <div className='home'>
       <div className='home__content'>
         <Navbar />
+        <div className='home__hero'>
+          <h2>Small solutions for small problems</h2>
+          <h1>Kueroom, a place to find rooms</h1>
+        </div>
+        <TimeSelect />
       </div>
     </div>
   );
